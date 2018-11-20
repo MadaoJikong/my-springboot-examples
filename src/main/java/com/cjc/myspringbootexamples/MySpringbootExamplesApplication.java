42450ac40481f0023dbcdb01ce8ffb004112f36e
chengjunchao <414076881@qq.com>
2018-11-20 09:39:58 +0800

    start.spring.io上构建项目

---
+++ src/main/java/com/cjc/myspringbootexamples/MySpringbootExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.cjc.myspringbootexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootExamplesApplication.class, args);
+	}
+}
